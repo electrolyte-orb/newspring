e6e9e1e5629fc04de0f5b958a125157c50966f63
Electrolyte Orb <electrolyte.spdf@outlook.com>
2024-05-09 18:14:22 +0530

    Linux

--- app/Appshell.tsx
+++ app/Appshell.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ReactNode, useEffect } from "react";
+import { useEffect } from "react";
 import { Anchor, AppShell, Container, Flex, Text } from "@mantine/core";
 import Link from "next/link";
 import classes from "./Appshell.module.css";
@@ -8,107 +8,108 @@ import { useAtom } from "jotai";
 import { useHydrateAtoms } from "jotai/utils";
 import { getUserAtom, setUserAtom, userAtom } from "./store";
 import { createClient } from "@/lib/client";
-import { User } from "@supabase/supabase-js";
+import type { User } from "@supabase/supabase-js";
+import type { ReactNode } from "react";
 import Image from "next/image";
 
 const footerLinks = [
-  { label: "Account", href: "/app/settings" },
-  { label: "Github", href: "https://github.com/electrolyte-orb" },
-  { label: "Home", href: "/" },
+	{ label: "Account", href: "/app/settings" },
+	{ label: "Github", href: "https://github.com/electrolyte-orb" },
+	{ label: "Home", href: "/" },
 ];
 
 export default function Appshell({
-  children,
-  userFromServer,
+	children,
+	userFromServer,
 }: {
-  children: ReactNode;
-  userFromServer: User | null;
+	children: ReactNode;
+	userFromServer: User | null;
 }) {
-  useHydrateAtoms([[userAtom, userFromServer]]);
+	useHydrateAtoms([[userAtom, userFromServer]]);
 
-  const [user] = useAtom(getUserAtom);
-  const [, setUser] = useAtom(setUserAtom);
+	const [user] = useAtom(getUserAtom);
+	const [, setUser] = useAtom(setUserAtom);
 
-  const supabase = createClient();
-  useEffect(() => {
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((e, session) => {
-      setUser(session ? session?.user : null);
-    });
+	const supabase = createClient();
+	useEffect(() => {
+		const {
+			data: { subscription },
+		} = supabase.auth.onAuthStateChange((e, session) => {
+			setUser(session ? session?.user : null);
+		});
 
-    () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+		() => {
+			subscription.unsubscribe();
+		};
+	}, [supabase.auth.onAuthStateChange, setUser]);
 
-  return (
-    <AppShell
-      header={{ height: 60, offset: true }}
-      footer={{ height: 100, offset: false }}
-    >
-      <AppShell.Header>
-        <Container h="100%">
-          <Flex align="center" h="100%" justify="space-between">
-            <Anchor
-              component={Link}
-              href="/"
-              underline="never"
-              c="white"
-              size="lg"
-              fw="bold"
-            >
-              Newspring
-            </Anchor>
-            {user !== null && (
-              <Link href="/app/settings" className={classes.imageLink}>
-                <Image
-                  alt="user-profile-pic"
-                  width={32}
-                  height={32}
-                  src={user.user_metadata.avatar_url}
-                />
-              </Link>
-            )}
-          </Flex>
-        </Container>
-      </AppShell.Header>
-      <AppShell.Main>{children}</AppShell.Main>
-      <AppShell.Footer bg="dark.7" pos="static">
-        <Container my={16}>
-          <Text size="sm" fw="Medium">
-            ©️ Newspring 2024
-          </Text>
+	return (
+		<AppShell
+			header={{ height: 60, offset: true }}
+			footer={{ height: 100, offset: false }}
+		>
+			<AppShell.Header>
+				<Container h="100%">
+					<Flex align="center" h="100%" justify="space-between">
+						<Anchor
+							component={Link}
+							href="/"
+							underline="never"
+							c="white"
+							size="lg"
+							fw="bold"
+						>
+							Newspring
+						</Anchor>
+						{user !== null && (
+							<Link href="/app/settings" className={classes.imageLink}>
+								<Image
+									alt="user-profile-pic"
+									width={32}
+									height={32}
+									src={user.user_metadata.avatar_url}
+								/>
+							</Link>
+						)}
+					</Flex>
+				</Container>
+			</AppShell.Header>
+			<AppShell.Main>{children}</AppShell.Main>
+			<AppShell.Footer bg="dark.7" pos="static">
+				<Container my={16}>
+					<Text size="sm" fw="Medium">
+						©️ Newspring 2024
+					</Text>
 
-          <Flex gap="md" mt={16}>
-            <FooterLinks />
-          </Flex>
-        </Container>
-      </AppShell.Footer>
-    </AppShell>
-  );
+					<Flex gap="md" mt={16}>
+						<FooterLinks />
+					</Flex>
+				</Container>
+			</AppShell.Footer>
+		</AppShell>
+	);
 }
 
 function CustomAnchor({
-  children,
-  href,
+	children,
+	href,
 }: {
-  children: ReactNode;
-  href: string;
+	children: ReactNode;
+	href: string;
 }) {
-  return (
-    <Anchor component={Link} href={href} classNames={{ root: classes.root }}>
-      {children}
-    </Anchor>
-  );
+	return (
+		<Anchor component={Link} href={href} classNames={{ root: classes.root }}>
+			{children}
+		</Anchor>
+	);
 }
 
 function FooterLinks() {
-  return (
-    <>
-      {footerLinks.map(({ label, href }, index) => (
-        <CustomAnchor children={label} href={href} key={index} />
-      ))}
-    </>
-  );
+	return (
+		<>
+			{footerLinks.map(({ label, href }, index) => (
+				<CustomAnchor children={label} href={href} key={index} />
+			))}
+		</>
+	);
 }
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -7,6 +7,9 @@ export const revalidate = 0;
 export default async function App() {
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
+  const { data, error } = await supabase.from("contact_view").select("*");
+
+  console.log(data, error);
 
   return (
     <Container>
