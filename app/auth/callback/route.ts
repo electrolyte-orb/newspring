9fdef7691508305b6208ecbf8a45e64d486db00c
Electrolyte Orb <electrolyte.spdf@outlook.com>
2024-08-02 09:41:34 +0530

    route handler to edge

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,42 +2,43 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
 
-export const revalidate = 0;
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
 
 export async function GET(request: Request) {
-  const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code");
-  const next = searchParams.get("next") ?? "/";
+	const { searchParams, origin } = new URL(request.url);
+	const code = searchParams.get("code");
+	const next = searchParams.get("next") ?? "/";
 
-  if (code) {
-    const cookieStore = cookies();
-    const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        global: {
-          fetch: (...args) => fetch(...args),
-        },
+	if (code) {
+		const cookieStore = cookies();
+		const supabase = createServerClient(
+			process.env.NEXT_PUBLIC_SUPABASE_URL!,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			{
+				global: {
+					fetch: (...args) => fetch(args[0], { ...args[1], cache: "no-store" }),
+				},
 
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value;
-          },
-          set(name: string, value: string, options: CookieOptions) {
-            cookieStore.set({ name, value, ...options });
-          },
-          remove(name: string, options: CookieOptions) {
-            cookieStore.delete({ name, ...options });
-          },
-        },
-      },
-    );
+				cookies: {
+					get(name: string) {
+						return cookieStore.get(name)?.value;
+					},
+					set(name: string, value: string, options: CookieOptions) {
+						cookieStore.set({ name, value, ...options });
+					},
+					remove(name: string, options: CookieOptions) {
+						cookieStore.delete({ name, ...options });
+					},
+				},
+			},
+		);
 
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
-    if (!error) {
-      return NextResponse.redirect(`${origin}${next}`);
-    }
-  }
+		const { error } = await supabase.auth.exchangeCodeForSession(code);
+		if (!error) {
+			return NextResponse.redirect(`${origin}${next}`);
+		}
+	}
 
-  return NextResponse.redirect(`${origin}/auth/auth-code-error`);
+	return NextResponse.redirect(`${origin}/auth/auth-code-error`);
 }
